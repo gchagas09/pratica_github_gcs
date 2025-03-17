6775a4fc29522a7cc7ca0f7b48e31f673a20c5fd
Pedro-Fleck <pedrocfleck2001@gmail.com>
2025-03-17 19:30:23 -0300

    implementados itens a,b e c

--- Search.java
+++ Search.java
@@ -1,4 +1,79 @@
+import java.util.ArrayList;
+
 public class Search {
 
-    
+    public static int contarOcorrencias(ArrayList<Integer> lista, Integer elemento){
+        int count = 0;
+
+        for (int item : lista){
+            if (item==elemento){
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String args[]){
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(2);
+        lista.add(4);
+        lista.add(2);
+
+        System.out.println("existem " + contarOcorrencias(lista, 2) +" ocorrencias");
+        System.out.println("Lista possui elementos repetidos? " + hasRepeat(lista));
+        System.out.println(qtdRepeat(lista));
+    }
+
+    public static boolean hasRepeat(ArrayList<Integer> l) {
+        for(int i = 0;i<l.size();i++){
+            Integer e =l.get(i);
+            for(int j = i+1;j<l.size();j++){
+                if(e.equals(l.get(j))){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static int qtdRepeat(ArrayList<Integer> l) {
+        ArrayList<Integer> repeated = new ArrayList<>();
+        for(int i = 0;i<l.size();i++){
+            Integer e =l.get(i);
+            for(int j = i+1;j<l.size();j++){
+                if(e.equals(l.get(j))){
+                    if(!repeated.contains(e)){
+                        repeated.add(e);
+                    }
+
+                }
+            }
+        }
+        return repeated.size();
+    }
 }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
